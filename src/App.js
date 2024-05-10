f108ab009b04077230236505ce6f009011ee45f5
Naitik <naitik2002patel@gmail.com>
2024-05-10 11:37:29 +0530

    clean index.js by updateing routers in app.js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./App.css";
+
 import Navbar from "./components/Navbar";
 import ProductCard from "./components/ProductCard";
 import Home from "./pages/Home";
 import { ChakraProvider } from "@chakra-ui/react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-function App() {
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Categories from "./pages/Categories";
+import Cart from "./pages/Cart";
+
+const AppLayout = () => {
   return (
-    <ChakraProvider>
-    <div className="App">
+    <>
       <Navbar />
-      {/* <h1>Hello</h1> */}
-      <Home />
-    </div>
-    </ChakraProvider>
+      <Outlet />
+    </>
+  );
+};
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/home", element: <Home /> },
+      { path: "/categories", element: <Categories /> },
+      { path: "/cart", element: <Cart /> },
+    ],
+  },
+]);
+export function App() {
+  return (
+    <React.StrictMode>
+      <ChakraProvider>
+        <RouterProvider router={router} />
+      </ChakraProvider>
+    </React.StrictMode>
   );
 }
-
--- src/index.js
+++ src/index.js
@@ -1,38 +1,8 @@
 import * as React from "react";
+import { App } from "./App";
 import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import Home from "./pages/Home";
-import { ChakraProvider } from "@chakra-ui/react";
-import Navbar from "./components/Navbar";
-import { Outlet } from "react-router-dom";
-import Cart from "./pages/Cart";
-import Categories from "./pages/Categories";
-const AppLayout = () => {
-  return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
-  );
-};
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <AppLayout />,
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/home", element: <Home /> },
-      { path: "/categories", element: <Categories /> },
-      { path: "/cart", element: <Cart /> },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <RouterProvider router={router} />
-    </ChakraProvider>
-  </React.StrictMode>
+  <App/>
 );
